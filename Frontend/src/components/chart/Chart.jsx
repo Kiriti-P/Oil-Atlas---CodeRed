1b94b62388a9f95bcdc2f35e9276f2f8a3f2fad4
Kiriti-P <kiriti2003@gmail.com>
2022-10-30 09:07:25 -0500

    Chart Change

--- Frontend/src/components/chart/Chart.jsx
+++ Frontend/src/components/chart/Chart.jsx
@@ -1,39 +1,56 @@
 import './chart.scss'
- import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import React from 'react';
+import CanvasJSReact from '../../canvasjs.react';
+import data from './DataBase/ProducedCrudeData.json';
+//var CanvasJS = CanvasJSReact.CanvasJS;
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+// import api 
+// import Api from '/api';
+// import { useEffect, useState } from 'react'
 
-
-
-const data = [
-  { name: 'January', total: 1200 },
-  { name: 'February', total: 3000 },
-  { name: 'March', total: 1002 },
-  { name: 'April', total: 900 },
-  { name: 'May', total: 500 },
-  { name: 'June', total: 3200 },
-  { name: 'July', total: 1200 },
- 
-];
-
-const Chart = () => {
-  return (
-    <div className='chart'>
-      <div className="title">Last 6 Months Revenue</div>
-        <ResponsiveContainer width="100%" aspect={2/1}>
-        <AreaChart width={'100%'} height={250} data={data} margin={{ top: 0, right: 0, left: 0, bottom: 0 }}>
-            <defs>
-              <linearGradient id="total" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
-                <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
-              </linearGradient>
-            </defs>
-            <XAxis dataKey="name" />
-            <CartesianGrid strokeDasharray="3 3" className='chartGrid' />
-            <Tooltip />
-            <Area type="monotone" dataKey="total" stroke="#8884d8" fillOpacity={1} fill="url(#total)" />     
-        </AreaChart>                 
-      </ResponsiveContainer>
-    </div>
-  )
+let crudeData = [];
+for (let i = 0; i < data["Data 1"].length; i++) {
+	if (i%20 === 0) {
+		crudeData.push({
+			label: data["Data 1"].at(i).Date,
+			y: data["Data 1"].at(i)['U.S. Field Production of Crude Oil (Thousand Barrels)']
+	})
+	}
 }
 
-export default Chart
+
+const Chart = () => {
+	const options = {
+		animationEnabled: true,
+		exportEnabled: true,
+		theme: "light2", // "light1", "dark1", "dark2"
+		title:{
+			text: "U.S. Total Crude Production"
+		},
+		axisY: {
+			title: "Crude Produced\n(Thousand Barrels)",
+			//suffix: "%"
+		},
+		axisX: {
+			title: "Year",
+			//prefix: "W",
+			//interval: 2
+		},
+		
+		data: [{
+			type: "column",
+			toolTipContent: "Week {label}: {y}",
+			dataPoints: crudeData
+		}]
+	}
+	return (
+		<div className='chart'>
+			<CanvasJSChart options = {options}
+				/* onRef={ref => this.chart = ref} */
+			/>
+			{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
+		</div>
+	);
+}
+//module.exports = Chart;
+export default Chart;
